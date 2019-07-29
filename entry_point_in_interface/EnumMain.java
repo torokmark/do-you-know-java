050061f466278bde98c75fb14a2bc4c16610369f
Mark Torok <torok.marko@gmail.com>
2019-07-29 22:22:11 +0200

    Add entry point in interface, main arguments as varargs

---
+++ entry_point_in_interface/EnumMain.java
@@ -0,0 +1,8 @@
+
+enum Main {
+    MAIN;
+    Main() { }
+    public static void main(String[] args) {
+        System.out.println("Enum");
+    }
+}
---
+++ entry_point_in_interface/InterfaceMain.java
@@ -0,0 +1,6 @@
+
+interface InterfaceMain {
+    public static void main(String[] args) {
+        System.out.println("in interface");
+    }
+}
