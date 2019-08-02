2c0bcb8fa066e9e0d301fdb980703c250756196a
Mark Torok <torok.marko@gmail.com>
2019-08-02 20:12:38 +0200

    Add underscores in numeric values

---
+++ underscores_in_numeric_literals/Main.java
@@ -0,0 +1,12 @@
+
+class Main {
+    public static void main(String[] args) {
+        int ____j = 0x5_4;
+        byte b = 1_2_7;
+        short s = 32_767;
+        int i = 1_000_000;
+        long l = 1_000_000_000_000L;
+        float f = 1_000.5f;
+        double d = 1_000.5d;
+    }
+}
