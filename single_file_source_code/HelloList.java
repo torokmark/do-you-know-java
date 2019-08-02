5af65660f3b3b41c9719d4a835c5c0f075b28d6e
Mark Torok <torok.marko@gmail.com>
2019-08-02 11:38:48 +0200

    Add cp related source

---
+++ single_file_source_code/HelloList.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import com.google.common.primitives.Ints;
+
+class HelloList {
+    public static void main(String[] args) {
+        List<Integer> nums = Ints.asList(1, 2, 0, 4);
+    }
+}
