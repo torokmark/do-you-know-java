612d99dd0a9ef9a39fe97aeb9f67220029e31882
Torok Mark <torok.marko@gmail.com>
2019-11-18 17:02:47 +0100

    Add one more ... time

---
+++ one_more_more_more_time/Main.java
@@ -0,0 +1,20 @@
+
+class Main {
+    public static void main(String[] args) {
+        int n = 0;
+        
+        n = 1 - - - - - - 1;
+        System.out.println(n);
+
+        n = 1 + - + - + - 1;
+        System.out.println(n);
+
+        //n = 1 * * * * * * 2;
+        //System.out.println(n);
+    
+        boolean b = false;
+
+        //b = true ! ! ! ! ! ! ! false;
+        System.out.println(b);
+    }
+}
