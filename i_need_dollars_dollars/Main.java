523c8d98d4e42d990dbb809d76eb73730472baf6
Torok Mark <torok.marko@gmail.com>
2020-04-26 18:13:19 +0200

    Add I need dollars dollars

---
+++ i_need_dollars_dollars/Main.java
@@ -0,0 +1,21 @@
+class $ {
+    private String $;
+
+    public void $(String $) {
+        this.$ = $;
+    }
+
+    public String $() {
+        return this.$;
+    }
+}
+
+public class Main
+{
+    public static void main(String[] args) {
+        $ $ = new $();
+        $.$("$");
+        System.out.println($.$());
+    }
+}
+
