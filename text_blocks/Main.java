e48cca8c2b332a3db76492d972f539fe2cefd39c
Torok Mark <torok.marko@gmail.com>
2020-05-01 18:33:49 +0200

    Add text blocks

---
+++ text_blocks/Main.java
@@ -0,0 +1,11 @@
+
+class Main {
+    public static void main(String[] args) {
+        String text = """
+                      block
+                      of
+                      text
+                       """;
+        System.out.println(text);
+    }
+}
