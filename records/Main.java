ce591ebf9dcba27d6ff91e1ffd3db5aedbc28cc6
Torok Mark <torok.marko@gmail.com>
2020-05-23 20:15:06 +0200

    Add records

---
+++ records/Main.java
@@ -0,0 +1,28 @@
+
+record Person(String name, int age) implements Comparable<Person> {
+    Person() {
+        this("default", 0);
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return 0;
+    }
+
+}
+
+class Main {
+
+    private record Box(int a, int b, int c) {}
+
+    public static void main(String[] args) {
+        record Box(int a, int b, int c) {}
+        Person person = new Person("jancsi", 24);        
+        System.out.println(person);
+        // person.setName("julcsa"); // compile time error
+        System.out.println(person);
+
+        Person other = new Person();
+        System.out.println(other);
+    }
+}
