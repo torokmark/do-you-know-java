72c46dcafeee680276b328a434c636acde34deb4
Torok Mark <torok.marko@gmail.com>
2021-05-13 07:15:25 +0200

    Add additional thread to daemon demo

--- daemon_thread/Main.java
+++ daemon_thread/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Thread t = new Daemon();
-        // t.setDaemon(true);
+        t.setDaemon(true);
         t.start();
+        //new Daemon().start();
         System.out.println("t started");
         Thread.sleep(3500);
         System.out.println("ended");
