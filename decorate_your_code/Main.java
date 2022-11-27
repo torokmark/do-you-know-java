c3328cb6394383a703e144110c750528b98c169f
Torok Mark <torok.marko@gmail.com>
2022-11-27 13:20:47 +0100

    Add decorate your code

---
+++ decorate_your_code/Main.java
@@ -0,0 +1,11 @@
+public interface Main {
+    public static void main(String[] args) {
+        decoreate();
+    }
+
+  private static void decoreate() {
+        int i = 1 + + + + + + + 1;
+        int j = 1 + - + - + - + 1;
+        int k = 1 - - - - - - - 1;
+    }
+}
